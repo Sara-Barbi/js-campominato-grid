7df8e769d11cf25de865c34a009bf4eaf4880e0a
Sara-Barbi <barbisara02@gmail.com>
2021-12-20 16:58:18 +0100

    svuoto go ad ogni click

--- scrtipt.js
+++ scrtipt.js
@@ -1,22 +1,24 @@
-let selezione = document.getElementById("myselect").value;
 let go = document.getElementById("start");
 
 
 go.addEventListener ("click", function(){
-
+    let selezione = document.getElementById("myselect").value;
+    let block = document.getElementById("block");
+    block.innerHTML = "";
+    
 if(selezione=="easy"){
     for(let i=0; i<100 ; i++){
-       document.getElementById("block").innerHTML += `<div class="quadrato  item-${i}"> a</div>`;
+       block.innerHTML += `<div class="quadrato  item-${i}"> a</div>`;
     }
 
 }else if(selezione=="medium"){
     for(let index=0; index<81 ; index++){
-       document.getElementById("block").innerHTML += `<div class="quadr  item-${index}"> a</div>`;
+       block.innerHTML += `<div class="quadr  item-${index}"> a</div>`;
     }
 
 }else if(selezione=="hard"){
     for(let j=0; j<49 ; j++){
-       document.getElementById("block").innerHTML += `<div class="qua  item-${j}"> a</div>`;
+       block.innerHTML += `<div class="qua  item-${j}"> a</div>`;
     }
   }
 })
